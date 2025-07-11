f005dd42df1dfb1ff8bdf580cf46ed356f3ee12c
mczkzk <k.mochizuki.0409@gmail.com>
2025-07-11 22:53:56 +0900

    Configure external OpenAPI spec and add localhost server

--- handson/orders/app.py
+++ handson/orders/app.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
+from pathlib import Path
+import yaml
 
-app = FastAPI(debug=True)
+# マイクロサービス
+app = FastAPI(debug=True, openapi_url="/openapi/orders.json", docs_url="/docs/orders")
+
+# API ドキュメントを読み込む
+oas_doc = yaml.safe_load(Path(__file__).parent / "../openapi.yaml").read_text()
+
+# FastAPI の OpenAPI ドキュメントを上書きする
+app.openapi = lambda: oas_doc
 
 from orders.api import api
